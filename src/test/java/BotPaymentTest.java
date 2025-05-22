69dac5fd37f2c42190629cf4994e62e384d03bb3
Whilker Henrique dos Santos Silva <whilker_santos@hotmail.com>
2025-05-22 18:36:45 -0300

    Create BotPaymentTest.java

---
+++ src/test/java/BotPaymentTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import pkg.banco.*;
+import pkg.driver.Driver;
+
+public class BotPaymentTest {
+
+    @Test
+    public void testConcurrentBotPayments() throws InterruptedException {
+
+       Driver driver1 = new Driver("driver1", "driver1");
+       Driver driver2 = new Driver("driver2", "driver2");
+       Driver driver3 = new Driver("driver3", "driver3");
+
+
+
+
+        
+    }
+}
