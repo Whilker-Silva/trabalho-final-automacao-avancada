fdcbc8199e8a304531fdda26a41896416ffdbc03
Whilker Henrique dos Santos Silva <whilker_santos@hotmail.com>
2025-07-01 21:34:03 -0300

    Versão final

--- src/main/java/reconciliacao/Principal.java
+++ src/main/java/reconciliacao/Principal.java
@@ -2,7 +2,7 @@ package reconciliacao;
 
 public class Principal {
     public static void main(String[] args) {
-        Reconciliacao reconciliacao = new Reconciliacao( "data/Dados_simulacao.xlsx");
+        Reconciliacao reconciliacao = new Reconciliacao( "data/dados simulacao/Dados_simulacao.xlsx");
         reconciliacao.start();
     }
 }
--- src/main/java/simulacao/Principal.java
+++ src/main/java/simulacao/Principal.java
@@ -3,14 +3,16 @@ package simulacao;
 public class Principal {
 
     public static void main(String[] args) {
-
         try {
-
-            /* Simulão para realizar a reconcilição de dados */
-            EnvSimulator ev = new EnvSimulator(1, 10, true, true);
 
             /* Simulação com 100 drivers e 200 rotas */
             // EnvSimulator ev = new EnvSimulator(100, 200,false, false);
+
+            /* Simulação para realizar a reconcilição de dados */
+            // EnvSimulator ev = new EnvSimulator(1, 10, true, true);
+
+            /* Simulação para com um unico carro e sem trasito */
+            EnvSimulator ev = new EnvSimulator(1, 1, false, false);
 
             ev.start();
             ev.join();
